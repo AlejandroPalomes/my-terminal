17759ca1f02e44999b70827ede5c57afcd78194a
Alex Palomes <alejandropalomes@hotmail.com>
2020-06-04 11:41:54 +0200

    detect command

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-var root = [
-    {
+var root = [{
         "type": "archive",
         "name": "readme.txt",
         "pwd": "root/"
@@ -8,8 +7,7 @@ var root = [
         "type": "folder",
         "name": "src",
         "pwd": "root/",
-        "content": [
-            {
+        "content": [{
                 "type": "archive",
                 "name": "log.txt",
                 "pwd": "root/src"
@@ -25,13 +23,62 @@ var root = [
 ]
 
 var actualFolder = root;
+var actualFolderName = "root";
+
+$("#terminal__input").keydown(userAction);
+
+function userAction() {
+    // When the user sends a command
+    if (event.key == "Enter") {
+        // Printing the command on the terminal
+        $("#terminal__output").append($("<p>").text(actualFolderName + " $ " + $("#terminal__input").val()));
+        // Splitting the command for evaluation
+        var input = $("#terminal__input").val().split(" ");
+        console.log(input);
+
+        $(input).each((_,e)=>{
+            switch (e){
+                case "ls":
+                    //ls();
+                    console.log("ls chosen")
+                break
+                case "cd":
+                    cd(actualFolder, input[1]);
+                    console.log("cd chosen")
+                break
+        }
+        });
+        $('#terminal__input').val("");
+    }
+}
 
 function cd (actualFolder, nextFolder) {
-    actualFolder.forEach((file,index) => {
+    actualFolder.forEach((file, index) => {
         if (file.name == nextFolder && file.type == "folder") {
             actualFolder = actualFolder[index];
         } else {
-            console.log("Folder does not exist");
+            $("#terminal__output").append($("<p>")).text("Folder does not exist");
         }
     });
 }
+
+function ls(){
+    console.log("I'm in function ls")
+}
+
+// $(document).ready(
+//     function () {
+//         $("input").addClass("-real-textarea");
+//         //   $(".textarea-wrapper").append("<textarea class=\"hidden\"></textarea>");
+//         $(".textarea-wrapper textarea.hidden").keyup(
+//             function () {
+//                 $(".textarea-wrapper textarea.-real-textarea").val($(this).val());
+//             }
+//         );
+//         $(".textarea-wrapper textarea.-real-textarea").focus(
+//             function () {
+//                 $(this).parent().find("textarea.hidden").focus();
+//             }
+//         );
+//     }
+// )
