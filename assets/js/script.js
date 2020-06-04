e1f8c258f0418742ebd42509a61aa2eef056ae3a
Alex Palomes <alejandropalomes@hotmail.com>
2020-06-04 10:20:42 +0200

    localStorage idea

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var root = [
+    {
+        "type": "archive",
+        "name": "readme.txt",
+        "pwd": "root/"
+    },
+    {
+        "type": "folder",
+        "name": "src",
+        "pwd": "root/",
+        "content": [
+            {
+                "type": "archive",
+                "name": "log.txt",
+                "pwd": "root/src"
+            },
+            {
+                "type": "folder",
+                "name": "imgs",
+                "pwd": "root/src",
+                "content": []
+            }
+        ]
+    }
+]
